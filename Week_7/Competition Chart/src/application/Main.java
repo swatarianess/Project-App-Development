c375a9ee89bf214f2fdcf5dfaf6001b5617a6953
andro25 <candrej@ymail.com>
2016-12-20 11:40:55 +0100

    few changes

--- Week_7/Competition Chart/src/application/Main.java
+++ Week_7/Competition Chart/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,800,750);
+			Scene scene = new Scene(root,1040,750);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
