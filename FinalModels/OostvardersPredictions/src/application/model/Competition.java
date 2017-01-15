eb8a71e06e07de8259819449dc2523074f68c6fa
Steve <swatarianess@users.noreply.github.com>
2017-01-15 21:36:23 +0100

    Updated to better fit MVC and better coding conventions

--- FinalModels/OostvardersPredictions/src/application/model/Competition.java
+++ FinalModels/OostvardersPredictions/src/application/model/Competition.java
@@ -3,186 +3,133 @@ package application.model;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class Competition {
-
-    //Socket
-    private Socket s = new Socket();
-    private PrintWriter s_out;
-    private BufferedReader s_in;
+public class Competition{
 
     //Number of each species
-    private double nCow = 0;
-    private double nDeer = 0;
-    private double nHorse = 0;
-    private double nGoose = 0;
+    private double nCows = 0;
+    private double nDeers = 0;
+    private double nHorses = 0;
+    private double nGeese = 0;
 
     //Rate of reproduction for each animal
     private double rateCow = 0;
     private double rateDeer = 0;
     private double rateHorse = 0;
     private double rateGoose = 0;
+
+    //Initial Grass amount
     private double availableGrass = 22487514; //14173224
 
-    //Years to predict
-    private int predictionRange = 0;
+    //Consumptions
+    private double cowFoodConsumption;
+    private double horseFoodConsumption;
+    private double deerFoodConsumption;
+    private double gooseFoodConsumption;
 
-    //nCow, nDeer, nHorse,nGoose, RateCow, RateDeer, RateHorse, RateGoose, grassAvailable
+    //Variables for keeping time
+    private int currentYear = 0;
+
+    //nCows, nDeers, nHorses,nGeese, RateCow, RateDeer, RateHorse, RateGoose, grassAvailable
     private HashMap<Integer,Double[]> predictedStats = new HashMap<>();
 
     /**
-     * @param nCow The initial population of cows
+     * @param nCows The initial population of cows
      * @param nDeer The initial population of deer
-     * @param nHorse The initial population of horses
-     * @param nGoose The initial population of geece
-     * @param predictionRange The amount of years to predict
+     * @param nHorses The initial population of horses
+     * @param nGeese The initial population of geese
      */
-    public Competition(int nCow, int nDeer, int nHorse, int nGoose, int predictionRange){
-        this.nCow = nCow;
-        this.nDeer = nDeer;
-        this.nHorse = nHorse;
-        this.nGoose = nGoose;
-        this.predictionRange = predictionRange;
-        this.predictedStats.put(0, new Double[] {this.nCow, this.nDeer, this.nHorse, this.nGoose,
+    public Competition(int nCows, int nDeer, int nHorses, int nGeese){
+        this.nCows = nCows;
+        this.nDeers = nDeer;
+        this.nHorses = nHorses;
+        this.nGeese = nGeese;
+
+        cowFoodConsumption = 15 * 365 * nCows;
+        horseFoodConsumption = 9.4 * 365 * nHorses;
+        deerFoodConsumption = 2.5 * 365 * nDeer;
+        gooseFoodConsumption = 0.15 * 365 * nGeese;
+
+        predictedStats.put(0, new Double[] {this.nCows, this.nDeers, this.nHorses, this.nGeese,
                 0.00, 0.00, 0.00, 0.00, availableGrass});
-
-        System.out.println("No connection to server established");
-
     }
-
-    /**
-     * @param nCow The initial population of cows
-     * @param nDeer The initial population of deer
-     * @param nHorse The initial population of horses
-     * @param nGoose The initial population of geece
-     * @param predictionRange The amount of years to predict
-     * @param socket The socket to connect to the other application
-     */
-    public Competition(int nCow, int nDeer, int nHorse, int nGoose, int predictionRange, Socket socket){
-        this.nCow = nCow;
-        this.nDeer = nDeer;
-        this.nHorse = nHorse;
-        this.nGoose = nGoose;
-        this.predictionRange = predictionRange;
-        this.predictedStats.put(0, new Double[] {this.nCow, this.nDeer, this.nHorse, this.nGoose,
-                0.00, 0.00, 0.00, 0.00, availableGrass});
-
-        //Handles all the socket initialization
-        this.s = socket; // Sets socket variable to paramater's socket
-        if(s.isConnected()) { //Checks connection is established
-            try {
-                System.out.println("Connected to server: " + s.getInetAddress() + ":" + s.getPort()); // Prints Success message
-                this.s_out = new PrintWriter(s.getOutputStream(), true); //Initialize writer for socket
-                this.s_in = new BufferedReader(new InputStreamReader(s.getInputStream())); //Initialize reader for socket
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("No connection to server established");
-        }
-    }
-
 
     /**
      *  Predicts the population changes from the initial year to the last year provided
      */
     public void predictPopulations() throws IOException, InterruptedException {
-        waitUntilResponse();
         //Loop through each year
-        for(int currentYear = 1; currentYear < predictionRange+1 && availableGrass > 0; currentYear++){
+        currentYear++;
+        double rCow = 0.1588527691497652;
+        double kCow = 427;
+        double aCowDeer = 0.2832;
+        double aCowHorse = 0.9748;
+        double aCowGoose = 0.9401;
 
-            double cowFoodConsumption = 15 * 365 * nCow;
-            double horseFoodConsumption = 9.4 * 365 * nHorse;
-            double deerFoodConsumption = 2.5 * 365 * nDeer;
-            double gooseFoodConsumption = 0.15 * 365 * nGoose;
+        //For Deer
+        double rDeer = 0.372092211870399691;
+        double kDeer = 2000;
+        double aDeerCow = 0.6340;
+        double aDeerHorse = 0.6346;
+        double aDeerGoose = 0.6214;
 
-            double rCow = 0.1588527691497652;
-            double kCow = 427;
-            double aCowDeer = 0.2832;
-            double aCowHorse = 0.9748;
-            double aCowGoose = 0.9401;
-            rateCow = rCow * (nCow + rateCow)
-                    * (1 - ((nCow + (aCowDeer * nDeer) + (aCowHorse * nHorse) + (aCowGoose * nGoose)) / kCow));
+        //For Horses
+        double rHorse = 0.330062695413144696;
+        double kHorse = 868;
+        double aHorseCow = 1.010;
+        double aHorseDeer = 0.2937;
+        double aHorseGoose = 0.97;
 
-            double rDeer = 0.372092211870399691;
-            double kDeer = 2000;
-            double aDeerCow = 0.6340;
-            double aDeerHorse = 0.6346;
-            double aDeerGoose = 0.6214;
+        //For Geese
+        double rGoose = 0.0449709691497654;
+        double kGoose = 2000;
+        rateCow = rCow * (nCows + rateCow)
+                * (1 - ((nCows + (aCowDeer * nDeers) + (aCowHorse * nHorses) + (aCowGoose * nGeese)) / kCow));
 
-            rateDeer = rDeer * (nDeer + rateDeer)
-                    * (1 - ((nDeer + (aDeerCow * nCow) + (aDeerHorse * nHorse) + (aDeerGoose * nGoose)) / kDeer));
+        rateDeer = rDeer * (nDeers + rateDeer)
+                * (1 - ((nDeers + (aDeerCow * nCows) + (aDeerHorse * nHorses) + (aDeerGoose * nGeese)) / kDeer));
 
-            double rHorse = 0.330062695413144696;
-            double kHorse = 868;
-            double aHorseCow = 1.010;
-            double aHorseDeer = 0.2937;
-            double aHorseGoose = 0.97;
+        rateHorse = rHorse * (nHorses + rateHorse)
+                * (1 - ((nHorses + (aHorseCow * nCows) + (aHorseDeer * nDeers) + (aHorseGoose * nGeese)) / kHorse));
 
-            rateHorse = rHorse * (nHorse + rateHorse)
-                    * (1 - ((nHorse + (aHorseCow * nCow) + (aHorseDeer * nDeer) + (aHorseGoose * nGoose)) / kHorse));
+        rateGoose = rGoose * (nGeese + rateGoose)
+                * (1 - ((nGeese + (aCowGoose * nCows) + (aDeerGoose * nDeers) + (aHorseGoose * nHorses)) / kGoose));
 
-            double rGoose = 0.0449709691497654;
-            double kGoose = 2000;
+        nCows += rateCow;
+        nDeers += rateDeer;
+        nHorses += rateHorse;
+        nGeese += rateGoose;
 
-            rateGoose = rGoose * (nGoose + rateGoose)
-                    * (1 - ((nGoose + (aCowGoose * nCow) + (aDeerGoose * nDeer) + (aHorseGoose * nHorse)) / kGoose));
+        //grass grows on average 4 inch a month, which means yearly in kg = 55065840
+        availableGrass += 1627900;//based on 1cm a year
+        availableGrass = (int) (availableGrass - (cowFoodConsumption + horseFoodConsumption
+                + deerFoodConsumption + gooseFoodConsumption));
 
-            nCow = nCow + rateCow;
-            nDeer = nDeer + rateDeer;
-            nHorse = nHorse + rateHorse;
-            nGoose = nGoose + rateGoose;
-
-            //grass grows on average 4 inch a month, which means yearly in kg = 55065840
-            availableGrass += 1627900;//based on 1cm a year
-            availableGrass = (int) (availableGrass - (cowFoodConsumption + horseFoodConsumption
-                    + deerFoodConsumption + gooseFoodConsumption));
-
-            if (nCow < 1) {
-                nCow = (double) 0;
-                rateCow = 0;
-            }
-            if (nDeer < 1) {
-                nDeer = (double) 0;
-                rateDeer = 0;
-            }
-            if (nHorse < 1) {
-                nHorse = (double) 0;
-                rateHorse = 0;
-            }
-            if (nGoose < 1) {
-                nGoose = (double) 0;
-                rateGoose = 0;
-            }
-
-            predictedStats.put(currentYear, new Double[] {nCow, nDeer, nHorse, nGoose,
-                    rateCow, rateDeer, rateHorse, rateGoose, availableGrass});
-
-            if(s.isConnected()){
-                System.out.println(generateJson((double) currentYear, nGoose, availableGrass));
-//              s_out.println(generateJson((double)currentYear,nGoose,availableGrass);
-                waitUntilResponse();
-
-            }
-
+        if (nCows < 1) {
+            nCows = (double) 0;
+            rateCow = 0;
         }
 
-/*        try {
-            s.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (nDeers < 1) {
+            nDeers = (double) 0;
+            rateDeer = 0;
         }
-*/
 
+        if (nHorses < 1) {
+            nHorses = (double) 0;
+            rateHorse = 0;
+        }
+
+        if (nGeese < 1) {
+            nGeese = (double) 0;
+            rateGoose = 0;
+        }
+
+        predictedStats.put(currentYear, new Double[] {nCows, nDeers, nHorses, nGeese,
+                rateCow, rateDeer, rateHorse, rateGoose, availableGrass});
     }
 
     /**
@@ -192,46 +139,28 @@ public class Competition {
         return predictedStats;
     }
 
-    /**
-     * @param year The current year of prediction
-     * @param geese The number of geese in the current year
-     * @param grass The amount of grass in the current year
-     * @return Returns a parsed string in Json format
-     */
-    private String generateJson(Double year, Double geese, Double grass){
-        Gson g = new GsonBuilder().create();
-        HashMap<String,Double> jsonData = new HashMap<>();
-
-        jsonData.put("Iteration",year);
-        jsonData.put("Geese",geese);
-        jsonData.put("Grass",grass);
-
-        return g.toJson(jsonData);
+    public double getAvailableGrass() {
+        return availableGrass;
     }
 
-    /**
-     * @param s The string to parse from Json Object to a double to initialize variables
-     * @return Returns a double from the string paramater entered
-     */
-    private double parseDataReceived(String s){
-        Gson g = new GsonBuilder().create();
-//        g.fromJson(s);
-        return 0.00;
+    public double getnCows() {
+        return nCows;
     }
 
-    /**
-     * @throws InterruptedException Thrown if interrupted before completing
-     * @throws IOException Thrown if cannot read response from socket
-     */
-    private void waitUntilResponse() throws InterruptedException, IOException {
-        int counter = 0;
-        do{
-            Thread.sleep(250);
-            //Counter for testing purposes
-            counter++;
-            if(counter>=2)
-                break;
-        }while (!s_in.ready());
+    public double getnDeers() {
+        return nDeers;
+    }
+
+    public double getnHorses() {
+        return nHorses;
+    }
+
+    public int getCurrentYear() {
+        return currentYear;
+    }
+
+    public double getnGeese() {
+        return nGeese;
     }
 
 }
--- FinalModels/OostvardersPredictions/src/application/view/Controller.java
+++ FinalModels/OostvardersPredictions/src/application/view/Controller.java
@@ -1,10 +1,14 @@
 package application.view;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URL;
 import java.text.DecimalFormat;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -34,35 +38,37 @@ public class Controller implements Initializable {
 	@FXML
 	public RadioButton totalRadiobutton;
 
-	@SuppressWarnings("rawtypes")
-	private XYChart.Series cowSeries = new XYChart.Series<>();
-	@SuppressWarnings("rawtypes")
-	private XYChart.Series deerSeries = new XYChart.Series<>();
-	@SuppressWarnings("rawtypes")
-	private XYChart.Series horseSeries = new XYChart.Series<>();
-	@SuppressWarnings("rawtypes")
-	private XYChart.Series gooseSeries = new XYChart.Series<>();
+	@FXML
+	private TextField cattleInputTextField;
+	@FXML
+	private TextField horseInputTextField;
+	@FXML
+	private TextField deerInputTextField;
+	@FXML
+	private TextField geeseInputTextField;
+	@FXML
+	private TextField yearsInputTextField;
+
 
 	@FXML
-	private TextField cattleInput;
+	private Button computeButton;
 	@FXML
-	private TextField horseInput;
+	private Button showHistoryButton;
 	@FXML
-	private TextField deerInput;
-	@FXML
-	private TextField geeseInput;
-	@FXML
-	private TextField yearsInput;
-	@FXML
-	private Button compute;
-	@FXML
-	private Button showHistory;
-	@FXML
-	private Button clearData;
+	private Button clearDataButton;
+
+
 	@FXML
 	private LineChart<Number, Number> lineChart;
 
-	private Socket socket = new Socket();
+	private XYChart.Series cowSeries = new XYChart.Series<>();
+	private XYChart.Series deerSeries = new XYChart.Series<>();
+	private XYChart.Series horseSeries = new XYChart.Series<>();
+	private XYChart.Series gooseSeries = new XYChart.Series<>();
+
+	private Socket sckt = new Socket();
+	private PrintWriter s_out;
+	private BufferedReader s_in;
 
 	// clear the graph data
 	@FXML
@@ -72,9 +78,9 @@ public class Controller implements Initializable {
 		deerSeries.getData().clear();
 		horseSeries.getData().clear();
 		gooseSeries.getData().clear();
-		if (clearData.isArmed()) {
-			compute.setDisable(false);
-			showHistory.setDisable(false);
+		if (clearDataButton.isArmed()) {
+			computeButton.setDisable(false);
+			showHistoryButton.setDisable(false);
 		}
 	}
 	
@@ -107,7 +113,6 @@ public class Controller implements Initializable {
 			}
 		}
 	}
-
 
 	@SuppressWarnings("unchecked")
 	@FXML
@@ -147,36 +152,42 @@ public class Controller implements Initializable {
 	private void handleCompute(ActionEvent event) throws InterruptedException, IOException {
 		if (isInputValid()) {
 			// Converting TextField to String
-			int cows = Integer.parseInt(cattleInput.getText());
-			int horses = Integer.parseInt(horseInput.getText());
-			int deers = Integer.parseInt(deerInput.getText());
-			int geese = Integer.parseInt(geeseInput.getText());
-			int years = Integer.parseInt(yearsInput.getText());
+			int cows = Integer.parseInt(cattleInputTextField.getText());
+			int horses = Integer.parseInt(horseInputTextField.getText());
+			int deers = Integer.parseInt(deerInputTextField.getText());
+			int geese = Integer.parseInt(geeseInputTextField.getText());
+			int years = Integer.parseInt(yearsInputTextField.getText());
+
 			Map<Integer,Double[]> data;
 
-//			Competition c = new Competition(cows,deers,horses,geese,years);
+			Competition c = new Competition(cows,deers,horses,geese);
+			System.out.println(generateJson((double) c.getCurrentYear(), c.getnGeese(), c.getAvailableGrass()));
 
-			Competition c = new Competition(cows,deers,horses,geese,years,socket);
-			c.predictPopulations();
+			for(int y =0; y < years; y++) {
+				waitUntilResponse();
+				c.predictPopulations();
+				System.out.println(generateJson((double) c.getCurrentYear(), c.getnGeese(), c.getAvailableGrass()));
+			}
+
 			data = c.getMap();
 
-			if(compute.isArmed()){
-				compute.setDisable(true);
-				showHistory.setDisable(true);
+			if(computeButton.isArmed()){
+				computeButton.setDisable(true);
+				showHistoryButton.setDisable(true);
 			}
 
 			System.out.println("==== Oostvarders Prediction System =====");
 			for(int year : data.keySet()){ //Iterates the Keys
 				/*
-				data.get(year)[0] = nCow
+				data.get(year)[0] = nCows
 				data.get(year)[1] = nDeer
-				data.get(year)[2] = nHorse
-				data.get(year)[3] = nGoose
+				data.get(year)[2] = nHorses
+				data.get(year)[3] = nGeese
 				data.get(year)[4] = rateCow
 				data.get(year)[5] = rateDeer
 				data.get(year)[6] = rateHorse
 				data.get(year)[7] = rateGoose
-				data.get(year)[8] = tempGrass
+				data.get(year)[8] = grassAvailable
 				*/
 				double nCow = data.get(year)[0];
 				double nDeer = data.get(year)[1];
@@ -188,9 +199,8 @@ public class Controller implements Initializable {
 				horseSeries.getData().add(new XYChart.Data<>(year, nHorse));
 				gooseSeries.getData().add(new XYChart.Data<>(year, nGoose));
 				DecimalFormat df = new DecimalFormat("#,###");
-				System.out.println(String.format("Year: %d ",year) + "Food left: " + df.format(data.get(year)[8].intValue()) + " kg.");
+				System.out.println(String.format("Year: %d ",year) + " | Food Available: " + df.format(data.get(year)[8].intValue()) + " kg.");
 			}
-
 			System.out.println("========================================");
 			lineChart.getData().addAll(cowSeries, horseSeries, deerSeries, gooseSeries);
 		}
@@ -209,29 +219,30 @@ public class Controller implements Initializable {
 		@SuppressWarnings("unused")
 		int valid;
 		try{
-			valid = Integer.parseInt(horseInput.getText());
-			valid = Integer.parseInt(cattleInput.getText());
-			valid = Integer.parseInt(deerInput.getText());
-			valid = Integer.parseInt(geeseInput.getText());
-			valid = Integer.parseInt(yearsInput.getText());
+			valid = Integer.parseInt(horseInputTextField.getText());
+			valid = Integer.parseInt(cattleInputTextField.getText());
+			valid = Integer.parseInt(deerInputTextField.getText());
+			valid = Integer.parseInt(geeseInputTextField.getText());
+			valid = Integer.parseInt(yearsInputTextField.getText());
 		}catch (NumberFormatException ex) {
 			errorMessage += "Please enter a valid number\n";
 		}
-		if (horseInput.getText() == null || horseInput.getText().length() == 0) {
+		if (horseInputTextField.getText() == null || horseInputTextField.getText().length() == 0) {
 			errorMessage += "Please enter horses input\n";
 		}
-		if (cattleInput.getText() == null || cattleInput.getText().length() == 0) {
+		if (cattleInputTextField.getText() == null || cattleInputTextField.getText().length() == 0) {
 			errorMessage += "Please enter cattle input\n";
 		}
-		if (deerInput.getText() == null || deerInput.getText().length() == 0) {
+		if (deerInputTextField.getText() == null || deerInputTextField.getText().length() == 0) {
 			errorMessage += "Please enter deer input\n";
 		}
-		if (geeseInput.getText() == null || geeseInput.getText().length() == 0) {
+		if (geeseInputTextField.getText() == null || geeseInputTextField.getText().length() == 0) {
 			errorMessage += "Please enter geese input\n";
 		}
-		if (yearsInput.getText() == null || yearsInput.getText().length() == 0) {
+		if (yearsInputTextField.getText() == null || yearsInputTextField.getText().length() == 0) {
 			errorMessage += "Please enter amount of years to predict";
 		}
+
 		if (errorMessage.length() == 0) {
 			return true;
 		} else {
@@ -245,7 +256,6 @@ public class Controller implements Initializable {
 			return false;
 		}
 	}
-
 
 	// display historical data
 	@SuppressWarnings({ "unchecked" })
@@ -358,10 +368,9 @@ public class Controller implements Initializable {
 		deerSeries.getData().add(new XYChart.Data<>(2015, 3200.0));
 
 		// draw all charts
-		if (showHistory.isArmed()) {
-			compute.setDisable(true);
-			showHistory.setDisable(true);
-
+		if (showHistoryButton.isArmed()) {
+			computeButton.setDisable(true);
+			showHistoryButton.setDisable(true);
 		}
 		lineChart.getData().addAll(cowSeries, horseSeries, deerSeries);
 	}
@@ -372,7 +381,6 @@ public class Controller implements Initializable {
 		alert.setTitle("About us");
 		alert.setHeaderText("Creators of this project");
 		alert.setContentText("Buaron Tal\nCholodov Andrej\nNieuwenhuis Jens\nAndreicha Semida\nAdu Stephen");
-
 		alert.showAndWait();
 	}
 
@@ -399,16 +407,74 @@ public class Controller implements Initializable {
 		lineChart.setAnimated(false);
 
 		//Socket initialization
+		initializeConnection("google.com",80);
+	}
+
+	private void initializeConnection(String host, int port){
 		try {
-			socket.connect(new InetSocketAddress("google.com",80));
+			sckt.connect(new InetSocketAddress(host,port));
 		} catch (IOException e) {
-			System.out.println("Could not connect to: " + socket.getInetAddress());
+			System.out.println("Could not connect to: " + sckt.getInetAddress());
 			e.printStackTrace();
 		}
 
-		if(socket.isConnected()){
-			System.out.println("Successfully connected to: " + socket.getInetAddress() + ":" + socket.getPort());
+		if(sckt.isConnected()) { //Checks connection is established
+			try {
+				System.out.println("Connected to server: " + sckt.getInetAddress() + ":" + sckt.getPort()); // Prints Success message
+				this.s_out = new PrintWriter(sckt.getOutputStream(), true); 									//Initialize writer for sckt
+				this.s_in = new BufferedReader(new InputStreamReader(sckt.getInputStream()));					//Initialize reader for sckt
+			} catch (IOException e){
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("No connection to server established");
 		}
+	}
+
+	/**
+	 * @throws InterruptedException Thrown if interrupted before completing
+	 * @throws IOException Thrown if cannot read response from sckt
+	 */
+	private void waitUntilResponse() throws InterruptedException, IOException {
+		if(sckt.isConnected()) {
+			int counter = 0;
+			do {
+				Thread.sleep(250);
+				//Counter for testing purposes
+				counter++;
+				if (counter >= 2)
+					break;
+			} while (!s_in.ready());
+		} else {
+			System.out.println("Connection not established to server.");
+		}
+	}
+
+
+	/**
+	 * @param year The current year of prediction
+	 * @param geese The number of geese in the current year
+	 * @param grass The amount of grass in the current year
+	 * @return Returns a parsed string in Json format
+	 */
+	private String generateJson(Double year, Double geese, Double grass){
+		Gson g = new GsonBuilder().create();
+		HashMap<String,Double> jsonData = new HashMap<>();
+
+		jsonData.put("Iteration",year);
+		jsonData.put("Geese",geese);
+		jsonData.put("Grass",grass);
+
+		return g.toJson(jsonData);
+	}
+
+	/**
+	 * @param s The string to parse from Json Object to a double to initialize variables
+	 * @return Returns a double from the string paramater entered
+	 */
+	private double parseDataReceived(String s){
+		Gson g = new GsonBuilder().create();
+		return 0.00;
 	}
 
 }
